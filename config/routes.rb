1550bf4710fb59044fcb12ce7dcba157707e93c1
Angie Duhard <angie.duhard@gmail.com>
2023-06-14 12:41:05 +0200

    yelp app with rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'restaurants/new'
+  get 'restaurants/create'
+  get 'restaurants/index'
+  get 'restaurants/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create, :index]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+names = ['La Rotonde', 'Queen Victoria', "Mama's Kitchen", 'Blue Jay', "Paul's"]
+addresses = [ 'Hyde Park LONDON', "Times Square NEW YORK", '21 rue Haxo MARSEILLE', 'Rue du Panier MARSEILLE', "Avenue du Louvre PARIS"]
+Restaurant.create(name: "Monsieur WU", category: 'chinese', address: '18 rue Damrémont PARIS')
+5.times { Restaurant.create(name: names.sample, category: Restaurant::CATEGORIES.sample, address: addresses.sample) }
